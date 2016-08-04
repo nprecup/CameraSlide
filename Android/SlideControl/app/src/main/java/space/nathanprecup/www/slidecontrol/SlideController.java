7c81ee7cb843eb58c7f9a9002732b08732ffa601
nprecup <nprecup@gmail.com>
2016-08-04 16:59:21 -0700

    Created Android Studio Project

---
+++ Android/SlideControl/app/src/main/java/space/nathanprecup/www/slidecontrol/SlideController.java
@@ -0,0 +1,13 @@
+package space.nathanprecup.www.slidecontrol;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SlideController extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_slide_controller);
+    }
+}
